c40d56ccbf33aec70f2eb5512983678e4550a12a
Nima HeydariNasab <nima.heydari79@yahoo.com>
2018-03-27 12:15:11 +0000

    Apply fixes from StyleCI

--- example.php
+++ example.php
@@ -1,19 +1,17 @@
 <?php
 
-require "sNode.Class.php";
+require 'sNode.Class.php';
 
-$snode = new sNode("YOUR_API_KEY");
+$snode = new sNode('YOUR_API_KEY');
 
 // Getting files list
 print_r($snode->list());
 
 // Uploading a new file
-print_r($snode->upload("FILE_NAME"));
+print_r($snode->upload('FILE_NAME'));
 
 // Getting stats of a file
-print_r($snode->stats("FILE_ID"));
+print_r($snode->stats('FILE_ID'));
 
 // Deleting a file
-print_r($snode->delete("FILE_ID"));
-
-?>
+print_r($snode->delete('FILE_ID'));
